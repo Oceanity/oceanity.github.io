085288619eabc61d3137f780cbbe439fd890c244
Oceanity <github@oceanity.net>
2021-05-05 14:08:03 -0500

    Added Sounds Page

---
+++ stream/sounds.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Stream Sound Effects</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+
+<body>
+    <?php
+        echo "test";
+
+    ?>
+</body>
+
+</html>
