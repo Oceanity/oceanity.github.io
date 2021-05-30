dd6cd242dafa3d66971f06def44aba905fcdc511
Oceanity <github@oceanity.net>
2021-05-30 17:44:06 -0500

    Updated HTML

---
+++ stream/sounds/scripts.js
@@ -0,0 +1,51 @@
+// Generate
+const groups = document.getElementsByClassName("sfx-group");
+for (let group of groups)
+{
+    const children = group.getElementsByTagName("div"),
+        title = group.getAttribute("name"),
+        header = document.createElement("h2"),
+        list = document.createElement("ul");
+    let entries = [];
+    header.innerText = title;
+    // Create child elements
+    while (group.firstElementChild)
+    {
+        const child = group.firstElementChild,
+            name = child.getAttribute("name"),
+            text = child.innerText;
+
+        if (name && text)
+        {
+            const entry = document.createElement("li");
+            entry.innerHTML = `<pre>${ name }</pre> ${ text.replace(/(\[[^\]]+\])/ig, "<span>$1</span>") }`;
+            entries.push(entry);
+        }
+
+        group.removeChild(child);
+    }
+    group.append(header);
+    group.append(list);
+
+    for (let entry of entries)
+    {
+        list.append(entry);
+    }
+}
+// Copy on Click
+const pres = document.getElementsByTagName("pre");
+for (let pre of pres)
+{
+    pre.onclick = function (e) {
+        document.execCommand("copy");
+    }
+
+    pre.addEventListener("copy", function (e) {
+        e.preventDefault();
+        if (e.clipboardData)
+        {
+            e.clipboardData.setData("text/plain", this.textContent);
+            console.log(e.clipboardData.getData("text"));
+        }
+    });
+}
