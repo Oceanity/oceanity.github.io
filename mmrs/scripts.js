4f2545caf396bd470dbc21eefb82553d83a0feb3
Oceanity <github@oceanity.net>
2021-06-29 19:51:13 -0500

    Updated OBS Files

--- mmrs/scripts.js
+++ mmrs/scripts.js
@@ -1,10 +1,12 @@
 
 
-const playButtons = document.getElementsByClassName("play");
+const audioContainer = document.getElementById("audio-container"),
+    playButtons = document.getElementsByClassName("play");
 
 for (let btn of playButtons) {
     btn.addEventListener("click", function(e) {
         e.preventDefault();
         console.log(e.target.getAttribute("href"));
+        audioContainer.setAttribute("src", e.target.getAttribute("href"));
     });
 }
